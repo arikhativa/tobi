0824c00664e7d16b6bdb4fb78b24fe7734d57e0c
Arik Hativa <arikhativa@gmail.com>
2023-12-18 09:46:42 +0200

    PlayWithSimpleAnimation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
 
+
+export class MainComponent {
+    ngOnInit() {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                }
+                else {
+                    entry.target.classList.remove("show");
+                }
+
+            });
+        });
+
+        const hiddenElem = document.querySelectorAll(".hidden");
+        hiddenElem.forEach(e => observer.observe(e));
+    }
 }
